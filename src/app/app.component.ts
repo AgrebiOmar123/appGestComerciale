18e5e73807ae9925821bc7b9d0e08f2a96b0d89f
AgrebiOmar123 <oagrebi614@gmail.com>
2023-12-06 12:52:27 +0100

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ interface SideNavToggle {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'sidenav-with-multilevel-menu';
+  title = 'Gestion Comerciale';
 
   isSideNavCollapsed = false;
   screenWidth = 0;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MediaComponent } from './media/media.component';
+
+//nessecité de proje
 import { AppComponent } from './app.component';
 import { BodyComponent } from './body/body.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { StatisticsComponent } from './statistics/statistics.component';
-import { PagesComponent } from './pages/pages.component';
-import { MediaComponent } from './media/media.component';
 import { SettingsComponent } from './settings/settings.component';
 import { SublevelMenuComponent } from './sidenav/sublevel-menu.component';
-import { AchatsComponent } from './achats/achats.component';
-import { VentesComponent } from './ventes/ventes.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
@@ -26,8 +27,8 @@ import { VentesComponent } from './ventes/ventes.component';
     MediaComponent,
     SettingsComponent,
     SublevelMenuComponent,
-    AchatsComponent,
-    VentesComponent
+  
+  
   ],
   imports: [
     BrowserModule,
